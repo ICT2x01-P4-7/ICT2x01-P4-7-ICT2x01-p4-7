ec25f4cb8a8fdc1404964e0e46d407ea9be01934
retxedchua <72144948+retxedchua@users.noreply.github.com>
2021-11-13 15:12:29 +0800

    History

--- src/client/src/router/routes.js
+++ src/client/src/router/routes.js
@@ -12,7 +12,7 @@ const routes = [
     path: "/program",
     name: "program",
     component: ProgramPage,
-  },
+  }
 ];
 
 export default routes;
--- src/client/src/views/ProgramPage.vue
+++ src/client/src/views/ProgramPage.vue
@@ -5,12 +5,25 @@
       <button v-on:click="showCode()">Show JavaScript</button>
       <pre v-html="code"></pre>
     </p>
+    <p id="historybutton">
+      <button v-on:click="showHistory()">History</button>
+    </p>
+    <transition name="slide" appear>
+      <div class="modal" v-if="showModal">
+      <h1>Lorem Ipsum</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem provident explicabo accusamus laudantium voluptatum nobis sed nesciunt neque possimus molestiae?</p>
+      <button class="button" v-on:click="showModal = false">
+        Close Modal
+      </button>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 
 import BlocklyComponent from '../components/BlocklyComponent.vue'
+import HistoryComponent from 'components/HistoryComponent.vue' 
 import '../blocks/movement';
 
 import BlocklyJS from 'blockly/javascript';
@@ -18,7 +31,8 @@ import BlocklyJS from 'blockly/javascript';
 export default {
   name: 'app',
   components: {
-    BlocklyComponent
+    BlocklyComponent,
+    HistoryComponent
   },
   data(){
     return {
@@ -48,6 +62,9 @@ export default {
   methods: {
     showCode() {
       this.code = BlocklyJS.workspaceToCode(this.$refs["program"].workspace);
+    },
+    showHistory() {
+      showModal = true;
     }
   }
 }
@@ -90,4 +107,27 @@ html, body {
   width: 50%;
   height: 50%;
 }
+#historybutton {
+ appearance: none;
+ outline: none;
+ border: none;
+ background: none;
+ cursor: pointer;
+ 
+ display: inline-block;
+ padding: 15px 25px;
+ background-image: linear-gradient(to right, #CC2E5D, #FF5858);
+ border-radius: 8px;
+ 
+ color: #FFF;
+ font-size: 18px;
+ font-weight: 700;
+ 
+ box-shadow: 3px 3px rgba(0, 0, 0, 0.4);
+ transition: 0.4s ease-out;
+}
+ #historybutton
+ :hover {
+  box-shadow: 6px 6px rgba(0, 0, 0, 0.6);
+ }
 </style>
