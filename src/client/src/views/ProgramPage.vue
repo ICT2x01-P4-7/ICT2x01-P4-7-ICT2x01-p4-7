a57a6b24ab8fd8fbaf7eb77d6e14c74d9a0378d2
retxedchua <72144948+retxedchua@users.noreply.github.com>
2021-11-13 21:27:54 +0800

    Completed History Page

--- src/client/src/views/ProgramPage.vue
+++ src/client/src/views/ProgramPage.vue
@@ -5,12 +5,14 @@
       <button v-on:click="showCode()">Show JavaScript</button>
       <pre v-html="code"></pre>
     </p>
+    <HistoryComponent />
   </div>
 </template>
 
 <script>
 
 import BlocklyComponent from '../components/BlocklyComponent.vue'
+import HistoryComponent from '../components/HistoryComponent.vue'
 import '../blocks/movement';
 
 import BlocklyJS from 'blockly/javascript';
@@ -18,7 +20,8 @@ import BlocklyJS from 'blockly/javascript';
 export default {
   name: 'app',
   components: {
-    BlocklyComponent
+    BlocklyComponent,
+    HistoryComponent
   },
   data(){
     return {
@@ -48,6 +51,9 @@ export default {
   methods: {
     showCode() {
       this.code = BlocklyJS.workspaceToCode(this.$refs["program"].workspace);
+    },
+    modalToggle() {
+      this.$refs.HistoryComponent.modalToggle();
     }
   }
 }
@@ -75,6 +81,7 @@ html, body {
   background-color: beige;
 }
 
+
 #blockly1 {
   position: absolute;
   right: 0;
@@ -90,4 +97,40 @@ html, body {
   width: 50%;
   height: 50%;
 }
+
+#history {
+    position: relative;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    width: 100vw;
+    min-height: 100vh;
+    overflow-x: hidden;
+}
+
+.button {
+    appearance: none;
+    outline: none;
+    border: none;
+    background: none;
+    cursor: pointer;
+
+    display: inline-block;
+    padding: 15px 25px;
+    background-image: linear-gradient(to right, #CC2E5D, #FF5858);
+    border-radius: 8px;
+
+    color: #FFF;
+    font-size: 18px;
+    font-weight: 700;
+
+    box-shadow: 3px 3px rgba(0, 0, 0, 0.4);
+    transition: 0.4s ease-out;
+}
+.button
+:hover {
+    box-shadow: 6px 6px rgba(0, 0, 0, 0.6);
+ }
 </style>
