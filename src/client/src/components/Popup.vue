77e5f5591b6ff3dd1804114b4e2065f01ebc6984
jevan97 <60656658+jevan97@users.noreply.github.com>
2021-11-24 03:50:21 +0800

    changed way of implementing timer

---
+++ src/client/src/components/Popup.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class = "popup">
+      <div class="popup-inner">
+          <slot /> <!--  CONTENTS WITHIN <Popup> in App.vue will appear here-->
+        <button type="button" class="close" @click="$emit('close')">CLOSE POPUP</button>
+        </div>
+
+      </div>
+</template>
+
+<script>
+export default {
+    name: 'Popup', 
+    methods: {
+        close() {
+            this.$emit('close');
+        },
+    },
+};
+</script>
+
+<style>
+
+</style>
--- src/client/src/main.js
+++ src/client/src/main.js
@@ -4,6 +4,8 @@ import App from "./App.vue";
 import Routes from "./router/routes";
 import PincodeInput from "vue-pincode-input";
 
+
+
 Vue.config.productionTip = false;
 Vue.config.ignoredElements = [
   "field",
@@ -28,3 +30,26 @@ new Vue({
   render: (h) => h(App),
   router: router,
 }).$mount("#app");
+
+setInterval(updateCountdown, 1000);
+
+//Each 0.1 refers to 6 seconds.
+//Barred Login shall increment from 30 seconds -> 60 seconds -> 90 seconds
+//Respective startingMinutes values are 0.5, 1, 1.5
+const startingMinutes = 0.5;
+let time = startingMinutes * 60;
+
+const countDownElement = document.getElementById('countdown');
+
+function updateCountdown() {
+  const minutes = Math.floor(time/60);
+  let seconds = time % 60;
+  if (minutes == 0 && seconds == 0)
+  {
+    countDownElement.innerHTML = `${0}: ${0}`;
+    return;
+  }
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  countDownElement.innerHTML = `${minutes}: ${seconds}`;
+  time --;
+}
