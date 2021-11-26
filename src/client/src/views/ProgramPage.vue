3af5ade4d3d12b990f52d06774a13f1b1cc882c5
nevermyuk <nevermyuk@gmail.com>
2021-11-27 02:43:49 +0800

    add buttons for program page

--- src/client/src/views/ProgramPage.vue
+++ src/client/src/views/ProgramPage.vue
@@ -1,39 +1,62 @@
 <template>
-  <div id="app">
-    <BlocklyComponent id="blockly2" :options="options" ref="program"></BlocklyComponent>
-    <p id="code">
-      <button v-on:click="showCode()">Show JavaScript</button>
+  <b-container>
+    <b-row>
+      <b-col lg="2" class="pt-4"
+        ><b-button pill variant="primary" size="lg">Dashboard</b-button></b-col
+      >
+      <b-col lg="2" class="pt-4"
+        ><b-button pill variant="dark" size="lg">Tutorial</b-button></b-col
+      >
+      <b-col lg="2" class="pt-4"
+        ><b-button pill variant="secondary" size="lg">History</b-button></b-col
+      >
+      <b-col lg="2" class="pt-4"
+        ><b-button pill variant="info" size="lg">Select Map</b-button></b-col
+      >
+      <b-col lg="2" class="pt-4"
+        ><b-button pill variant="danger" size="lg">Reset PIN</b-button></b-col
+      >
+      <b-col lg="2" class="pt-4">
+        <b-button pill variant="success" size="lg" v-on:click="sendCode()"
+          >Send to car</b-button
+        >
+      </b-col>
+    </b-row>
+    <BlocklyComponent
+      id="blockly1"
+      :options="options"
+      ref="program"
+    ></BlocklyComponent>
+
+    <!---<p id="code">
       <pre v-html="code"></pre>
-    </p>
-  </div>
+    </p> ---->
+  </b-container>
 </template>
 
 <script>
+import BlocklyComponent from "../components/BlocklyComponent.vue";
+import "../blocks/movement";
 
-import BlocklyComponent from '../components/BlocklyComponent.vue'
-import '../blocks/movement';
-
-import BlocklyJS from 'blockly/javascript';
+import BlocklyJS from "blockly/javascript";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    BlocklyComponent
+    BlocklyComponent,
   },
-  data(){
+  data() {
     return {
-      code: '',
+      code: "",
       options: {
-        media: 'media/',
-        grid:
-          {
-            spacing: 25,
-            length: 3,
-            colour: '#ccc',
-            snap: true
-          },
-        toolbox:
-        `<xml>
+        media: "media/",
+        grid: {
+          spacing: 25,
+          length: 3,
+          colour: "#ccc",
+          snap: true,
+        },
+        toolbox: `<xml>
             <category name="Movement" colour="%{BKY_VARIABLES_HUE}">
               <block type="forward"></block>
               <block type="backward"></block>
@@ -41,27 +64,40 @@ export default {
               <block type="right"></block>
               <block type="repeat_direction"></block>
             </category>
-        </xml>`
-      }
-    }
+        </xml>`,
+      },
+    };
   },
   methods: {
-    showCode() {
+    hasWhiteSpace(s) {
+      return s.indexOf("\n") >= 0;
+    },
+    clearWorkSpace() {
+      BlocklyJS.mainWorkspace.clear();
+    },
+    sendCode() {
+      const code = BlocklyJS.workspaceToCode(this.$refs["program"].workspace);
+      console.log(this.hasWhiteSpace(code));
+      console.log(code);
+      if (this.hasWhiteSpace(code)) {
+        console.log("Please link all block commands together");
+      }
       this.code = BlocklyJS.workspaceToCode(this.$refs["program"].workspace);
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
 
-html, body {
+html,
+body {
   margin: 0;
 }
 
@@ -78,16 +114,8 @@ html, body {
 #blockly1 {
   position: absolute;
   right: 0;
-  top: 0;
-  width: 50%;
-  height: 50%;
-}
-
-#blockly2 {
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  width: 50%;
-  height: 50%;
+  top: 10rem;
+  width: 100%;
+  height: 80%;
 }
 </style>
